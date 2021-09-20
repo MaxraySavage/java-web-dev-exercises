54f56a60ea78940386ffa153ca25feb732b42c25
MaxraySavage <maxraysavage@gmail.com>
2021-09-20 14:19:09 -0400

    Completes Exercise 1

---
+++ src/exercises/RectangleArea.java
@@ -0,0 +1,14 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input integer rectangle length:");
+        int length = input.nextInt();
+        System.out.println("Please input integer rectangle width:");
+        int width = input.nextInt();
+        System.out.println("The rectangle area is: " + (length * width));
+    }
+}
